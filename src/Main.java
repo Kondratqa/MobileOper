38c02d12d3e3a3927d4eae4dc1746a89512a9355
kinomagnum <kinomagnum@gmail.com>
2024-05-28 11:26:03 +0300

    MobileOper1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int currentBalance = 100; // Начальная сумма на счету клиента
-        int topUpAmount = 5800; // Сумма пополнения счета
 
-        int bonusPer100Rubles = 1; // Бонус за каждые полные 100 рублей пополнения
+        double currentBalance = 100; // Начальная сумма на счету клиента
+        double topUpAmount = 4800; // Сумма пополнения счета
 
-        int bonus = (topUpAmount / 100) * bonusPer100Rubles; // Рассчитываем количество бонусных рублей
-        int totalBalance = currentBalance + topUpAmount + bonus; // Рассчитываем итоговый счет клиента
+        double bonusPer100Rubles = 1; // Бонус за каждые полные 100 рублей пополнения
+        double bonusThreshold = 1000; // Порог для начисления бонуса
+        double bonus = 0; // Количество бонусных рублей
+        if (topUpAmount > bonusThreshold) {
+            bonus = ((topUpAmount - bonusThreshold) / 100) * bonusPer100Rubles;
+        }
+        double totalBalance = currentBalance + topUpAmount + bonus; // Рассчитываем итоговый счет клиента
 
         System.out.println("Итоговая сумма на счету клиента: " + totalBalance + " рублей");
         System.out.println("Количество бонусных рублей: " + bonus + " рублей");
+
     }
 }
 
